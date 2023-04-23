382188d8ef061a8218e6665e017e59cc9c85d29e
Torben <torben.groetzinger@gmx.de>
2023-04-23 23:38:27 +0200

    Sorting is not right yet

--- Photo/TaskPhoto.cpp
+++ Photo/TaskPhoto.cpp
@@ -1,39 +1,74 @@
-//
-// Created by torbe on 20.04.2023.
-//
-
-#include "TaskPhoto.h"
+#include <iostream>
 #include <bits/stdc++.h>
-
 
 using namespace std;
 using i64 = int64_t;
 using u64 = uint64_t;
 
-//#define float nichBenutzen!;
+// #define float nichBenutzen!;
 #define double sachMalBistDuPlöt?;
 
 
-int main() {
     string inputName;
-    i64 x;
-    i64 y;
+    long long x;
+    long long y;
 
-    map<string, i64> positions;
+    
 
+struct Position {
+        int64_t x, y;
+        
+        bool operator<(const Position &p) const {
+            // return (x < p.x) || (x == p.x && y < p.y);
+            return (x/y < p.x/p.y) || (x/y == p.x/p.y && x < p.x) ;
+        }
+        
+        bool operator==(const Position &p) const {
+            return x == p.x && y == p.y;
+        }
+        
+        // string operator<<(const Position & p) const {
+        //     return (string) x << ", " << y;
+        // }
+    };
+    
+    map<Position, string> positions;
+    
+int main() {
+
+    
+    
+    
+    
     while (cin >> inputName) {
         cin >> x;
         cin >> y;
+        
+        cout << inputName << endl;
+        cout << x/y << endl;
 
         //calculate the angle of every person
         //put the into the map which autosorts them
 
-        positions[inputName] = x / y;
-
-        //maybe try to sort, if the same position, for the x value, so if the person which is nearer is first
-        cout << positions;
-
+        positions[Position{x, y}] =  inputName;
+         
+        
     }
+    
+    for (auto &[k,v] : positions) {
+        cout << k.x << ", " << k.y << v << endl;
+    }
+    
+    //   for (auto it = begin(positions); it != end(positions); it++) {
+    //       cout << it->first << " " << it->second << endl;
+    //   }
+    
 
+        
+        // for (int64_t i = 0; i < sizeof(positions); ++i) {
+        //     cout << positions[i] << " ";
+        // }
 
+    return 0;
 }
+  
